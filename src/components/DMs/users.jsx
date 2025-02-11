81ea613cd9b6f453b00ee76b5a29f560a134cdf3
Martina Di Paola <giovyp73@gmail.com>
2025-02-11 23:59:50 +0900

    send with enter

--- src/components/DMs/users.jsx
+++ src/components/DMs/users.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineAddComment } from "react-icons/md";
+import NewChat from "./newChat"; // Import the component properly
+
+let globalCaseType = 1; // Global variable for caseType
 
 const UserList = ({ users, onUserClick, ProfilePics }) => {
+  const [showNewChat, setShowNewChat] = useState(false); // State to control visibility of the newChat component
+
+  const handleOpenNewChat = () => {
+    globalCaseType = 2; // Update the global caseType
+    setShowNewChat(true); // Show the newChat component
+  };
+
   return (
     <div
       style={{
@@ -20,7 +30,7 @@ const UserList = ({ users, onUserClick, ProfilePics }) => {
           textAlign: "center",
           paddingBottom: "10px",
           borderBottom: "0.1px solid #B0BEC5",
-          display: "flex",   // Using flex to align items horizontally
+          display: "flex", // Using flex to align items horizontally
           justifyContent: "center", // Center the content
           alignItems: "center", // Vertically align items
         }}
@@ -33,45 +43,53 @@ const UserList = ({ users, onUserClick, ProfilePics }) => {
             marginBottom: "-5px", // Center the icon vertically
             cursor: "pointer", // Make it clickable
           }}
-          onClick={() => alert("change this")} // CHANGE THIS TO SOMETHING 
+          onClick={handleOpenNewChat} // Call the handler
         >
-          <MdOutlineAddComment size={20} /> {/* Font Awesome "add" icon from React Icons */}
+          <MdOutlineAddComment size={20} />
         </span>
       </h2>
-      <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
-        {users.map((user, index) => (
-          <li
-            key={index}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              padding: "10px",
-              cursor: "pointer",
-              backgroundColor: "#F4FAFF",
-              margin: "5px 0",
-              borderRadius: "3px",
-              color: "#000000",
-            }}
-            onClick={() => onUserClick(user)} // Call the function when the user is clicked
-          >
-            {/* Display user profile picture */}
-            <img
-              src={ProfilePics[user] || "https://via.placeholder.com/30"} // Use the dummy image URL based on user name
-              alt={user}
+
+      {/* Conditionally render the newChat component */}
+      {showNewChat ? (
+        <NewChat caseType={globalCaseType} followers={users} />
+      ) : (
+        <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+          {users.map((user, index) => (
+            <li
+              key={index}
               style={{
-                width: "30px",
-                height: "30px",
-                borderRadius: "50%",
-                marginRight: "10px",
+                display: "flex",
+                alignItems: "center",
+                padding: "10px",
+                cursor: "pointer",
+                backgroundColor: "#F4FAFF",
+                margin: "5px 0",
+                borderRadius: "3px",
+                color: "#000000",
               }}
-            />
-            {user}
-          </li>
-        ))}
-      </ul>
+              onClick={() => onUserClick(user)} // Call the function when the user is clicked
+            >
+              {/* Display user profile picture */}
+              <img
+                src={ProfilePics[user] || "https://via.placeholder.com/30"} // Use the dummy image URL based on user name
+                alt={user}
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "50%",
+                  marginRight: "10px",
+                }}
+              />
+              {user}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
 export default UserList;
+
+
 
